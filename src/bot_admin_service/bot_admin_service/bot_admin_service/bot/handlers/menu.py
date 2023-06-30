c77468f9556aef3fe07907088bf64985b0266239
coolworld2049 <coolworld2049@gmail.com>
2023-06-30 20:38:33 +0300

    bot

--- src/bot_admin_service/bot_admin_service/bot_admin_service/bot/handlers/menu.py
+++ src/bot_admin_service/bot_admin_service/bot_admin_service/bot/handlers/menu.py
@@ -10,13 +10,12 @@ from sqlalchemy.exc import IntegrityError
 
 from bot_admin_service import crud, schemas
 from bot_admin_service.bot.callbacks import MenuCallback
-from bot_admin_service.bot.loader import main_bot
 from bot_admin_service.db.session import get_db
 
 router = Router(name=__file__)
 
 
-async def start_cmd(user: User, state: FSMContext, message_id: int):
+async def start_cmd(user: User, state: FSMContext):
     try:
         async with get_db() as db:
             bot_user = await crud.bot_user.create(
@@ -24,15 +23,13 @@ async def start_cmd(user: User, state: FSMContext, message_id: int):
             )
         logger.info(f"{user.id} created")
     except IntegrityError as ie:
-        logger.warning(f"{user.id} not created")
-    with suppress(TelegramBadRequest):
-        await main_bot.delete_message(user.id, message_id - 1)
+        logger.info(f"{user.id} already exist")
     await state.clear()
 
 
 @router.message(Command("start"))
 async def start_message(message: Message, state: FSMContext):
-    await start_cmd(message.from_user, state, message.message_id)
+    await start_cmd(message.from_user, state)
 
 
 @router.callback_query(MenuCallback.filter(F.name == "start"))
@@ -42,4 +39,4 @@ async def start_callback(
 ):
     with suppress(TelegramBadRequest):
         await query.message.delete()
-    await start_cmd(query.from_user, state, query.message.message_id)
+    await start_cmd(query.from_user, state)
